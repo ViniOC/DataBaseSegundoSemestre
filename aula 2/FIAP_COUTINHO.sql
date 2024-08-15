bd8f270035f7f333ea1dc49b97d7daf2467d7c02
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-15 12:06:30 -0300

    aula 14

---
+++ aula 2/FIAP_COUTINHO.sql
@@ -0,0 +1,26 @@
+insert into T_RHSTU_PACIENTE(
+NM_PACIENTE,
+NR_CPF,
+NM_RG,
+DT_NASCIMENTO,
+FL_SEXO_BIOLOGICO,
+DS_ESCOLARIDADE,
+DS_ESTADO_CIVIL,
+NM_GRUPO_SANGUINEO,
+NR_ALTURA,
+NR_PESO
+)VALUES(
+'Vinicius coutinho',
+12312312332,
+'14.144.255-1',
+'10-OCT-03',
+'M',
+'superior',
+'solteiro',
+'o+',
+1.86,
+120
+)
+
+
+select * from T_RHSTU_PACIENTE
---
+++ aula 2/codigoSQL/apaga.sql
@@ -0,0 +1,37 @@
+DROP TABLE t_rhstu_bairro CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_cidade CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_consulta CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_consulta_forma_pagto CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_contato_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_email_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_endereco_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_estado CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_forma_pagamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_logradouro CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_medicamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_medico CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_paciente_plano_saude CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_plano_saude CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_prescicao_medica CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_telefone_paciente CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_tipo_contato CASCADE CONSTRAINTS;
+
+DROP TABLE t_rhstu_unid_hospitalar CASCADE CONSTRAINTS;
